21b3cdf4b56ee02889c8adbcf9f6014eb50f39c8
MEGAN IVEY <PRINTMEGANIVEY@GMAIL.COM>
2018-06-15 15:26:55 -0700

    finished next question

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
 // Here is the test for sumAndMultiply(); uncomment it to run it
 testSumAndMultiply(4, 7, 5);
 
-console.log(sumAndMultiply(4, 7, 5));
+// console.log(sumAndMultiply(4, 7, 5));
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
@@ -99,11 +99,22 @@ var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
 
+  var answer = sum(testArray[0], testArray[1]);
+  // console.log(answer);
+  var answerTwo = sum(answer[0],testArray[2]);
+  // console.log(answerTwo);
+var string = testArray + ' was passed in as an array of numbers, and ' + answerTwo[0] + ' is their sum.';
+// var stringTwo = 'is their sum.';
+console.log(string);
+// console.log(stringTwo);
+return [answerTwo[0], string];
+console.log(answerTwo[0]);
+// console.log(testArray);
 }
-
+ 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
